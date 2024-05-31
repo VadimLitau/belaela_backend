152e020e04e847c005c14aace5034ba44a43db02
VadimLitau <vadimlitau@yandex.ru>
2024-05-31 23:31:01 +0500

    experimetn

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import expres from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import "dotenv/config";
-import fileUpload from "express-fileupload";
-import routes from "./routes";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import "dotenv/config";
+// import fileUpload from "express-fileupload";
+// import routes from "./routes";
 
-dotenv.config();
-const PORT = 9000;
+// dotenv.config();
+// const PORT = 9000;
 // const DB_URL = "mongodb://localhost:27017/Belaela";
 // const DB_URL =
 // 	"mongodb+srv://fallenarh:XszYIY0foELGoH74@belaela.ovpjnkw.mongodb.net/Belaela";
@@ -16,26 +16,28 @@ const PORT = 9000;
 const app = expres();
 
 //нужно чтобы экспресс мог понимать json
-app.use(cors());
-app.use(expres.json());
-app.use(expres.static("static"));
-app.use(fileUpload());
-app.use("/api", routes);
+// app.use(cors());
+// app.use(expres.json());
+// app.use(expres.static("static"));
+// app.use(fileUpload());
+// app.use("/api", routes);
 
 app.get("/", (req, res) => {
-	console.log(req.query);
 	res.status(200).json("Kurwa Bobrik");
 });
+app.listen(9000, () => {
+	console.log("server kurwa started");
+});
 
-async function startApp() {
-	try {
-		// await mongoose.connect(DB_URL);
-		app.listen(PORT, () => {
-			console.log("server kurwa started");
-		});
-	} catch (e) {
-		console.log(e);
-	}
-}
+// async function startApp() {
+// 	try {
+// 		// await mongoose.connect(DB_URL);
+// 		app.listen(PORT, () => {
+// 			console.log("server kurwa started");
+// 		});
+// 	} catch (e) {
+// 		console.log(e);
+// 	}
+// }
 
-startApp();
+// startApp();
