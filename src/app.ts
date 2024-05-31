a792cd45fc465c81642ea18a137425490b02cb8e
VadimLitau <vadimlitau@yandex.ru>
2024-05-31 23:14:02 +0500

    edit port

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import fileUpload from "express-fileupload";
 import routes from "./routes";
 
 dotenv.config();
-const PORT = 5000;
+const PORT = 5001;
 // const DB_URL = "mongodb://localhost:27017/Belaela";
 // const DB_URL =
 // 	"mongodb+srv://fallenarh:XszYIY0foELGoH74@belaela.ovpjnkw.mongodb.net/Belaela";
