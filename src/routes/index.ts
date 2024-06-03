8c21bc6b62fe98b247fcb0c74c68f602b4a51aef
VadimLitau <vadimlitau@yandex.ru>
2024-06-03 22:06:35 +0500

    vercel_createProduct_test_3

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,24 @@
 import { Router } from "express";
 import productRouter from "./products";
+import mongoose from "mongoose";
 const router = Router();
+const DataSchema = new mongoose.Schema({
+	name: String,
+	value: String,
+});
 
+const Data = mongoose.model("Data", DataSchema);
+router.post("/data", async (req, res) => {
+	try {
+		const { name, value } = req.body;
+		const newData = new Data({ name, value });
+		await newData.save();
+		res.status(201).json(newData);
+	} catch (error) {
+		console.error("Error saving data:", error);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+});
 router.use("/products", productRouter);
 
 export default router;
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,26 +1,9 @@
 import { Router } from "express";
 import ProductsController from "../controllers/products";
-import mongoose from "mongoose";
-const DataSchema = new mongoose.Schema({
-	name: String,
-	value: String,
-});
 
-const Data = mongoose.model("Data", DataSchema);
 const router = Router();
 
 router.get("/", ProductsController.getProducts);
 router.post("/", ProductsController.createProduct);
 
-router.post("/data", async (req, res) => {
-	try {
-		const { name, value } = req.body;
-		const newData = new Data({ name, value });
-		await newData.save();
-		res.status(201).json(newData);
-	} catch (error) {
-		console.error("Error saving data:", error);
-		res.status(500).json({ message: "Internal Server Error" });
-	}
-});
 export default router;
