ab887128455bd33a07b0d53d763302421804a14c
VadimLitau <vadimlitau@yandex.ru>
2024-06-03 21:43:25 +0500

    vercel_createProduct_test_1

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -10,12 +10,17 @@ class ProductsController {
 	}
 
 	async createProduct(req: any, res: Response, next: NextFunction) {
-		try {
-			const post = await ProductService.create(req.body);
-			res.json(post);
-		} catch (e) {
-			res.status(500).json(e);
-		}
+		// try {
+		// 	const post = await ProductService.create(req.body);
+		// 	res.status(200).send(post);
+		// } catch (e) {
+		// 	res.status(500).json(e);
+		// }
+		ProductService.create(req.body)
+			.then((product) => res.status(200).send(product))
+			.catch((err) => {
+				next(err);
+			});
 	}
 }
 
